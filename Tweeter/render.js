63ea33b2ce61a175eff2d5d727fabdd31682360d
sana mahamid <sanaa_6-5.7@hotmail.com>
2020-11-09 12:54:12 +0200

    Tweeter project done

--- Tweeter/render.js
+++ Tweeter/render.js
@@ -2,18 +2,24 @@ const Renderer = function(){
     const createCommentsList = function(comments){
         let members =""
         for(let c of comments){
-            members+=`<li class="comment" data-id=${c.id}> ${c.text} </li>`
+            members+=`<div class="comment" data-id=${c.id}><span class="delete-comment">X</span> ${c.text} </div>`
         }
-        let list = `<ul id="comments">${members}</ul>`
+       let list = `<div id="comments">${members}</div>`
         return list
     }
     const renderPosts = function(posts){
         $("#posts").empty()
         for(let post of posts){
-            console.log(post)
             let comments = createCommentsList(post.comments)
-            let p = `<div class="post" data-id=${post.id}>${post.text}</div>`
-            $("#posts").append(p).append(comments)
+            let p = `<div class="post" data-id=${post.id}>${post.text}
+            ${comments}
+            <input type="text" placeHolder="Got something to say?"/>
+            <button class="add">Comment</button>
+            <div><button class="delete">Delete Post</button></div>
+            </div>`
+            $("#posts").append(p)
+
+
         }
     }
 
